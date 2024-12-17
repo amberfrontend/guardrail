7a3ae63764d8c50297122b3ecde2cc0c918b9e09
Amber Armstrong <hello.amber.armstrong@gmail.com>
2024-12-17 13:50:48 -0500

    chore(Storybook): Use sentence case

--- src/elements/Button/stories/Button.stories.ts
+++ src/elements/Button/stories/Button.stories.ts
@@ -3,28 +3,22 @@ import { fn } from '@storybook/test';
 
 import Button from '../Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Elements/Button',
   component: Button,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     type: { control: 'select' },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn(), onKeyDown: fn(), onTouchStart: fn() },
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const OverlayTrigger: Story = {
   args: {
     type: 'overlay-trigger',
@@ -32,6 +26,7 @@ export const OverlayTrigger: Story = {
     ariaExpanded: false,
     children: 'Stuff',
   },
+  name: 'Overlay trigger',
 };
 
 export const IconOnly: Story = {
@@ -41,4 +36,5 @@ export const IconOnly: Story = {
     icon: 'pause',
     size: 'medium',
   },
+  name: 'Icon only',
 };
